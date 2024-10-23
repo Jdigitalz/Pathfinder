ddcd23561ad7bb56e9bdd8511e53f58adabec876
Jdigital <110255465+Jdigitalz@users.noreply.github.com>
2024-10-23 01:34:48 +0000

    Update pathfinder.py
    Made errors that appear exit gracefully

--- pathfinder.py
+++ pathfinder.py
@@ -5,16 +5,18 @@ import subprocess as sb
 def main():
     try: 
         file = sys.argv[1]
+        if os.path.exists(file):
+            pass
+        else:
+            print(f"No file or path found '{file}'")
+            exit()
         cwd = os.getcwd()
         full_path = f"{cwd}/{file}" 
         print(full_path)
         print("copied to cilpboard")
         sb.run(f"wl-copy {full_path}", shell=True)
-    except IndexError: 
-        if file == None: 
-            print("No file or path provided.")
-        else: 
-            print(f"No file or path found '{file}'")
+    except Exception: 
+        print(f"File or path was provided.")
 
 if __name__ == "__main__": 
-    main(file)
+    main()
